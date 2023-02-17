ffa3eeb3798f3b81a9950728b6ac8769f47e6165
farpires <arpirez.fabio@gmail.com>
2023-02-17 04:47:28 -0300

    Add: store configuration has been added

--- App.jsx
+++ App.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import {Text} from 'react-native';
+import { Provider } from "react-redux";
+import {store} from './src/store';
 
 function App() {
-  return <Text>Rick & Morty</Text>;
+  return (
+    <Provider store={store} >
+      <Text>Rick & Morty</Text>
+    </Provider>
+  );
 }
 
 export default App;
